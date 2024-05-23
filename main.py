67f2ed26e0ac2d16c39ece8c92bf5f4b744a95b1
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-23 14:50:12 +0200

    added logging

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from pathlib import PurePosixPath, Path
 from shutil import make_archive
 from zipfile import ZipFile, ZIP_DEFLATED
 from datetime import datetime
+from time import time
+from math import floor
 
 with open("data.json") as file:
     data = load(file)
@@ -15,29 +17,56 @@ for backupDir in data["folder"]:
     targetExists = path.exists(backupDir["target"])
 
     if sourceIsNotEmpty and targetIsNotEmpty and sourceExists and targetExists:
-        targetName = PurePosixPath(backupDir["source"]).stem
-        targetFileName = targetName + "_" + datetime.today().strftime("(%Y-%m-%d %H:%M:%S.%f)")
-        targetFileNameWithExt = targetFileName + ".zip"
-        targetFullPath = backupDir["target"] + targetFileName
-        targetFullPathWithExt = targetFullPath + ".zip"
+        logFile = backupDir["target"] + ".log.txt"
 
-        # Create backup archive
-        make_archive(targetFullPath, "zip", backupDir["source"])
-        with ZipFile(targetFullPathWithExt, "a", compression=ZIP_DEFLATED) as archive:
-            archive.writestr("source_" + targetName + ".txt", backupDir["source"])
+        with open(logFile, "a") as logFile:
+            targetName = PurePosixPath(backupDir["source"]).stem
+            targetFileName = targetName + "_" + datetime.today().strftime("(%Y-%m-%d %H:%M:%S.%f)")
+            targetFileNameWithExt = targetFileName + ".zip"
+            targetFullPath = backupDir["target"] + targetFileName
+            targetFullPathWithExt = targetFullPath + ".zip"
 
-        # Create tracking file for the backups
-        trackingFile = backupDir["target"] + "." + backupDir["source"].replace("/", "%").replace("\\", "%") + ".txt"
-        if not path.isfile(trackingFile):
-            with open(trackingFile, "w") as file:
-                file.write(targetFileNameWithExt)
-        else:
-            with open(trackingFile, "r+") as file:
-                trackedBackups = file.read().splitlines()
-                trackedBackups.insert(0, targetFileNameWithExt)
-                if len(trackedBackups) > 5:
-                    fileToDelete = trackedBackups[-1]
-                    del trackedBackups[-1]
-                    Path(backupDir["target"] + fileToDelete).unlink()
-                file.seek(0)
-                file.write("\n".join(trackedBackups))
+            # Create backup archive
+            timeStart = time()
+            make_archive(targetFullPath, "zip", backupDir["source"])
+            with ZipFile(targetFullPathWithExt, "a", compression=ZIP_DEFLATED) as archive:
+                archive.writestr("source_" + targetName + ".txt", backupDir["source"])
+            timeEnd = time()
+            logFile.write(
+                datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                f"Backup of {backupDir["source"]} " +
+                f"created as {targetFileNameWithExt} " +
+                f"in {floor(timeEnd - timeStart)} seconds" +
+                "\n"
+            )
+
+            # Create tracking file for the backups
+            trackingFile = (
+                backupDir["target"] +
+                "." +
+                backupDir["source"].replace("/", "%").replace("\\", "%") +
+                ".txt"
+            )
+            if not path.isfile(trackingFile):
+                with open(trackingFile, "w") as file:
+                    file.write(targetFileNameWithExt)
+                    logFile.write(
+                        datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                        f"Created new tracking file for {backupDir["source"]}" +
+                        "\n"
+                    )
+            else:
+                with open(trackingFile, "r+") as file:
+                    trackedBackups = file.read().splitlines()
+                    trackedBackups.insert(0, targetFileNameWithExt)
+                    if len(trackedBackups) > 5:
+                        fileToDelete = trackedBackups[-1]
+                        del trackedBackups[-1]
+                        Path(backupDir["target"] + fileToDelete).unlink()
+                        logFile.write(
+                            datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                            f"Deleted old backup {fileToDelete} of {backupDir["source"]}" +
+                            "\n"
+                        )
+                    file.seek(0)
+                    file.write("\n".join(trackedBackups))
