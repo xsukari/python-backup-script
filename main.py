6252f9e90054d8d617c4396b4bc73844e1726873
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-23 19:02:45 +0200

    added permission error handling

--- main.py
+++ main.py
@@ -28,63 +28,74 @@ for backupDir in data["folder"]:
         logFile = backupDir["target"] + ".log.txt"
 
         with open(logFile, "a") as logFile:
-            targetName = PurePosixPath(backupDir["source"]).stem
-            targetFileName = targetName + "_" + datetime.today().strftime("(%Y-%m-%d %H:%M:%S.%f)")
-            targetFileNameWithExt = targetFileName + ".zip"
-            targetFullPath = backupDir["target"] + targetFileName
-            targetFullPathWithExt = targetFullPath + ".zip"
+            try:
+                targetName = PurePosixPath(backupDir["source"]).stem
+                targetFileName = targetName + "_" + datetime.today().strftime("(%Y-%m-%d %H:%M:%S.%f)")
+                targetFileNameWithExt = targetFileName + ".zip"
+                targetFullPath = backupDir["target"] + targetFileName
+                targetFullPathWithExt = targetFullPath + ".zip"
 
-            # Create backup archive
-            timeStart = time()
-            make_archive(targetFullPath, "zip", backupDir["source"])
-            with ZipFile(targetFullPathWithExt, "a", compression=ZIP_DEFLATED) as archive:
-                archive.writestr("source_" + targetName + ".txt", backupDir["source"])
-            timeEnd = time()
-            logMessage = (
-                datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
-                f"Backup of {backupDir['source']} " +
-                f"created as {targetFileNameWithExt} " +
-                f"in {floor(timeEnd - timeStart)} seconds" +
-                "\n"
-            )
-            if verbose:
-                print(logMessage)
-            logFile.write(logMessage)
+                # Create backup archive
+                timeStart = time()
+                make_archive(targetFullPath, "zip", backupDir["source"])
+                with ZipFile(targetFullPathWithExt, "a", compression=ZIP_DEFLATED) as archive:
+                    archive.writestr("source_" + targetName + ".txt", backupDir["source"])
+                timeEnd = time()
+                logMessage = (
+                    datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                    f"Backup of {backupDir['source']} " +
+                    f"created as {targetFileNameWithExt} " +
+                    f"in {floor(timeEnd - timeStart)} seconds" +
+                    "\n"
+                )
+                if verbose:
+                    print(logMessage)
+                logFile.write(logMessage)
 
-            # Create tracking file for the backups
-            trackingFile = (
-                backupDir["target"] +
-                "." +
-                backupDir["source"].replace("/", "%").replace("\\", "%") +
-                ".txt"
-            )
-            if not path.isfile(trackingFile):
-                with open(trackingFile, "w") as file:
-                    file.write(targetFileNameWithExt)
-                    logMessage = (
-                        datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
-                        f"Created new tracking file for {backupDir['source']}" +
-                        "\n"
-                    )
-                    if verbose:
-                        print(logMessage)
-                    logFile.write(logMessage)
-
-            else:
-                with open(trackingFile, "r+") as file:
-                    trackedBackups = file.read().splitlines()
-                    trackedBackups.insert(0, targetFileNameWithExt)
-                    if len(trackedBackups) > backupCount:
-                        fileToDelete = trackedBackups[-1]
-                        del trackedBackups[-1]
-                        Path(backupDir["target"] + fileToDelete).unlink()
+                # Create tracking file for the backups
+                trackingFile = (
+                    backupDir["target"] +
+                    "." +
+                    backupDir["source"].replace("/", "%").replace("\\", "%") +
+                    ".txt"
+                )
+                if not path.isfile(trackingFile):
+                    with open(trackingFile, "w") as file:
+                        file.write(targetFileNameWithExt)
                         logMessage = (
                             datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
-                            f"Deleted old backup {fileToDelete} of {backupDir['source']}" +
+                            f"Created new tracking file for {backupDir['source']}" +
                             "\n"
                         )
                         if verbose:
                             print(logMessage)
                         logFile.write(logMessage)
-                    file.seek(0)
-                    file.write("\n".join(trackedBackups))
+
+                else:
+                    with open(trackingFile, "r+") as file:
+                        trackedBackups = file.read().splitlines()
+                        trackedBackups.insert(0, targetFileNameWithExt)
+                        if len(trackedBackups) > backupCount:
+                            fileToDelete = trackedBackups[-1]
+                            del trackedBackups[-1]
+                            Path(backupDir["target"] + fileToDelete).unlink()
+                            logMessage = (
+                                datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                                f"Deleted old backup {fileToDelete} of {backupDir['source']}" +
+                                "\n"
+                            )
+                            if verbose:
+                                print(logMessage)
+                            logFile.write(logMessage)
+                        file.seek(0)
+                        file.write("\n".join(trackedBackups))
+
+            except PermissionError:
+                logMessage = (
+                    datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                    f"Permission denied for {backupDir['source']}, skipping" +
+                    "\n"
+                )
+                if verbose:
+                    print(logMessage)
+                logFile.write(logMessage)
