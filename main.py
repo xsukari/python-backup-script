2e653c31a060a174c5c08dadb70ac866c8c050f4
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-23 13:13:41 +0200

    - refined imports - changed variable naming - allow up to 5 backups of an individual backup source - added tracking file for backups of an individual backup source - delete oldest backup when there are more than 5

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
-import os
-import json
-import pathlib
-import shutil
-import zipfile
+from os import path
+from json import load
+from pathlib import PurePosixPath, Path
+from shutil import make_archive
+from zipfile import ZipFile, ZIP_DEFLATED
+from datetime import datetime
 
 with open("data.json") as file:
-    data = json.load(file)
+    data = load(file)
 
-for folder in data["folders"]:
-    sourceIsNotEmpty = folder["source"] != ""
-    targetIsNotEmpty = folder["target"] != ""
-    sourceExists = os.path.exists(folder["source"])
-    targetExists = os.path.exists(folder["target"])
+for backupDir in data["folder"]:
+    sourceIsNotEmpty = backupDir["source"] != ""
+    targetIsNotEmpty = backupDir["target"] != ""
+    sourceExists = path.exists(backupDir["source"])
+    targetExists = path.exists(backupDir["target"])
 
     if sourceIsNotEmpty and targetIsNotEmpty and sourceExists and targetExists:
-        targetName = pathlib.PurePosixPath(folder["source"]).stem
-        targetDir = folder["target"] + targetName
+        targetName = PurePosixPath(backupDir["source"]).stem
+        targetFileName = targetName + "_" + datetime.today().strftime("(%Y-%m-%d %H:%M:%S.%f)")
+        targetFileNameWithExt = targetFileName + ".zip"
+        targetFullPath = backupDir["target"] + targetFileName
+        targetFullPathWithExt = targetFullPath + ".zip"
 
-        shutil.make_archive(targetDir, "zip", folder["source"])
-        with zipfile.ZipFile(targetDir + ".zip", "a", compression=zipfile.ZIP_DEFLATED) as archive:
-            archive.writestr("source_" + targetName + ".txt", folder["source"])
+        # Create backup archive
+        make_archive(targetFullPath, "zip", backupDir["source"])
+        with ZipFile(targetFullPathWithExt, "a", compression=ZIP_DEFLATED) as archive:
+            archive.writestr("source_" + targetName + ".txt", backupDir["source"])
+
+        # Create tracking file for the backups
+        trackingFile = backupDir["target"] + "." + backupDir["source"].replace("/", "%").replace("\\", "%") + ".txt"
+        if not path.isfile(trackingFile):
+            with open(trackingFile, "w") as file:
+                file.write(targetFileNameWithExt)
+        else:
+            with open(trackingFile, "r+") as file:
+                trackedBackups = file.read().splitlines()
+                trackedBackups.insert(0, targetFileNameWithExt)
+                if len(trackedBackups) > 5:
+                    fileToDelete = trackedBackups[-1]
+                    del trackedBackups[-1]
+                    Path(backupDir["target"] + fileToDelete).unlink()
+                file.seek(0)
+                file.write("\n".join(trackedBackups))
