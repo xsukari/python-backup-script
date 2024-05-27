4b2c07dd97a035870bba28b9745a4a9e57d8fe59
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-27 13:23:33 +0200

    multiple changes: - defined functions for separate parts of the script to improve readability - when a permission error occurs, the empty zip will be deleted - created fallback function for when the aforementioned permission error occurs when creating the zip - capitalized important words in log messages to improve readability

--- main.py
+++ main.py
@@ -1,19 +1,144 @@
 from os import path
 from json import load
 from pathlib import PurePosixPath, Path
-from shutil import make_archive
+from shutil import make_archive, copytree, rmtree, Error as shutilError
 from zipfile import ZipFile, ZIP_DEFLATED
 from datetime import datetime
 from time import time
 from math import floor
 from argparse import ArgumentParser
 
-backupCount = 4
+def log(message):
+    if verbose:
+        print(message)
+    logFile.write(message)
 
+
+def createArchive(altSrc = None):
+    global logMessage
+
+    if altSrc is None:
+        src = backupDir["source"]
+
+    else:
+        src = altSrc
+
+    timeStart = time()
+    make_archive(targetFullPath, "zip", src)
+    with ZipFile(targetFullPathWithExt, "a", compression=ZIP_DEFLATED) as archive:
+        archive.writestr("source_" + targetName + ".txt", backupDir["source"])
+    timeEnd = time()
+
+    logMessage = (
+            datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+            f"BACKUP of {backupDir['source']} " +
+            f"CREATED as {targetFileNameWithExt} " +
+            f"in {floor(timeEnd - timeStart)} seconds" +
+            "\n"
+    )
+    log(logMessage)
+
+
+def createTrackingFile():
+    global logMessage, file
+
+    trackingFile = (
+        backupDir["target"] +
+        "." +
+        backupDir["source"].replace("/", "%").replace("\\", "%") +
+        ".txt"
+    )
+    if not path.isfile(trackingFile):
+        with open(trackingFile, "w") as file:
+            file.write(targetFileNameWithExt)
+
+            logMessage = (
+                    datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                    f"CREATED new tracking file for {backupDir['source']}" +
+                    "\n"
+            )
+            log(logMessage)
+
+    else:
+        with open(trackingFile, "r+") as file:
+            trackedBackups = file.read().splitlines()
+            trackedBackups.insert(0, targetFileNameWithExt)
+
+            if len(trackedBackups) > backupCount:
+                fileToDelete = trackedBackups[-1]
+                del trackedBackups[-1]
+                Path(backupDir["target"] + fileToDelete).unlink()
+
+                logMessage = (
+                        datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                        f"DELETED old backup {fileToDelete} of {backupDir['source']}" +
+                        "\n"
+                )
+                log(logMessage)
+
+            file.seek(0)
+            file.write("\n".join(trackedBackups))
+
+
+def tryFallback():
+    global logMessage
+
+    try:
+        copytree(backupDir["source"], targetFullPathTemp, dirs_exist_ok=True)
+
+    except shutilError:
+        logMessage = (
+                datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                f"FALLBACK: some files could not be copied" +
+                "\n"
+        )
+        log(logMessage)
+
+    try:
+        logMessage = (
+                datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                f"FALLBACK: Creating archive from {targetFullPathTemp}" +
+                "\n"
+        )
+        log(logMessage)
+
+        createArchive(targetFullPathTemp)
+        createTrackingFile()
+        # Delete temp files
+        rmtree(targetFullPathTemp)
+
+        logMessage = (
+                datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                f"FALLBACK: SUCCESSFUL for {targetFullPathTemp}" +
+                "\n"
+        )
+        log(logMessage)
+
+    except PermissionError:
+        logMessage = (
+                datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                f"FALLBACK: FAILED for {targetFullPathTemp}" +
+                "\n"
+        )
+        log(logMessage)
+
+        # Delete failed zip
+        Path(targetFullPathWithExt).unlink()
+
+
+# Script start
 parser = ArgumentParser(description="Python backup script")
-parser.add_argument("-v", "--verbose", action="store_true", required=False, help="Enable output to console with -v")
+parser.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    required=False,
+    help="Enable output to console with -v"
+)
 args = parser.parse_args()
+
 verbose = args.verbose
+backupCount = 4
 
 with open("data.json") as file:
     data = load(file)
@@ -34,68 +159,22 @@ for backupDir in data["folder"]:
                 targetFileNameWithExt = targetFileName + ".zip"
                 targetFullPath = backupDir["target"] + targetFileName
                 targetFullPathWithExt = targetFullPath + ".zip"
+                targetFullPathTemp = backupDir["target"] + "temp/" + targetFileName
 
-                # Create backup archive
-                timeStart = time()
-                make_archive(targetFullPath, "zip", backupDir["source"])
-                with ZipFile(targetFullPathWithExt, "a", compression=ZIP_DEFLATED) as archive:
-                    archive.writestr("source_" + targetName + ".txt", backupDir["source"])
-                timeEnd = time()
-                logMessage = (
-                    datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
-                    f"Backup of {backupDir['source']} " +
-                    f"created as {targetFileNameWithExt} " +
-                    f"in {floor(timeEnd - timeStart)} seconds" +
-                    "\n"
-                )
-                if verbose:
-                    print(logMessage)
-                logFile.write(logMessage)
-
-                # Create tracking file for the backups
-                trackingFile = (
-                    backupDir["target"] +
-                    "." +
-                    backupDir["source"].replace("/", "%").replace("\\", "%") +
-                    ".txt"
-                )
-                if not path.isfile(trackingFile):
-                    with open(trackingFile, "w") as file:
-                        file.write(targetFileNameWithExt)
-                        logMessage = (
-                            datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
-                            f"Created new tracking file for {backupDir['source']}" +
-                            "\n"
-                        )
-                        if verbose:
-                            print(logMessage)
-                        logFile.write(logMessage)
-
-                else:
-                    with open(trackingFile, "r+") as file:
-                        trackedBackups = file.read().splitlines()
-                        trackedBackups.insert(0, targetFileNameWithExt)
-                        if len(trackedBackups) > backupCount:
-                            fileToDelete = trackedBackups[-1]
-                            del trackedBackups[-1]
-                            Path(backupDir["target"] + fileToDelete).unlink()
-                            logMessage = (
-                                datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
-                                f"Deleted old backup {fileToDelete} of {backupDir['source']}" +
-                                "\n"
-                            )
-                            if verbose:
-                                print(logMessage)
-                            logFile.write(logMessage)
-                        file.seek(0)
-                        file.write("\n".join(trackedBackups))
+                createArchive()
+                createTrackingFile()
 
             except PermissionError:
                 logMessage = (
-                    datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
-                    f"Permission denied for {backupDir['source']}, skipping" +
-                    "\n"
+                        datetime.today().strftime("%Y-%m-%d %H:%M:%S - ") +
+                        f"Permission DENIED for {backupDir['source']}, FALLING BACK to copying files" +
+                        "\n"
                 )
-                if verbose:
-                    print(logMessage)
-                logFile.write(logMessage)
+                log(logMessage)
+
+                # Delete failed zip
+                Path(targetFullPathWithExt).unlink()
+                zipFailed = True
+
+            if zipFailed:
+                tryFallback()
